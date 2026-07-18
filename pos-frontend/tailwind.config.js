b3b15c6ee78cbe650fd0ff50810ab6055062b6e9
kakdakhiev <kakdakhiev09@gmail.com>
2026-07-18 15:50:08 +0700

    Add khmer font

--- pos-frontend/tailwind.config.js
+++ pos-frontend/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
-        khmer: ['Hanuman', 'Inter', 'sans-serif'],
+        khmer: ['Khmer OS Battambang', 'Hanuman', 'Siemreap', 'Battambang', 'Inter', 'sans-serif'],
       },
     },
   },
